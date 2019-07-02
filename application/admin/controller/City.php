8ab5a6de5b3453b1bdb09c9391e62c72e41feced
JHR <1873645345@qq.com>
2019-07-02 22:26:02 +0800

    就差极光推送和计划任务了

--- application/admin/controller/City.php
+++ application/admin/controller/City.php
@@ -10,11 +10,8 @@ class City extends Base
     {
         $code = substr($code, 0,4);
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/'. substr($code, 0,2) . '/' . $code . '.html');curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_URL, 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/'. substr($code, 0,2) . '/' . $code . '.html');curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $data = curl_exec($curl);curl_close($curl);$data = mb_convert_encoding($data, 'UTF-8', 'GBK');
-//        if(in_array($code,['4604'])) {
-//            return [];
-//        }
 // 裁头
         $offset = @mb_strpos($data, 'countytr',2000,'GBK');
 
@@ -34,26 +31,26 @@ class City extends Base
         $out = $out[0];
 // 某个城市
         $list = [];
-        for ($j=0; $j < count($out) ; $j++) {
-            $list[] = [
-                'code'=> $out[$j],
-                'name'=> $out[++$j],
-                'pcode' => $code,
-                'level' => 3
-            ];
+        foreach ($out as $k=>$v) {
+            if($k%2 == 0) {
+                $list[] = [
+                    'code' => $out[$k],
+                    'name' => $out[$k+1],
+                    'pcode' => $code,
+                    'level' => 3
+                ];
+            }
         }
-//        halt($list);
-        unset($list[0]);
-//        $res = Db::table('mp_city')->insertAll($list);
-//        halt($res);
-
+        if(!in_array($code,[1101,1201,3101,5001])) {
+            unset($list[0]);
+        }
         return $list;
     }
 
     public function getCity($code)
     {
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/' . $code . '.html');curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_URL, 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/' . $code . '.html');curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $data = curl_exec($curl);
         curl_close($curl);
         $data = mb_convert_encoding($data, 'UTF-8', 'GBK');
@@ -65,83 +62,64 @@ class City extends Base
         $data = mb_substr($data, 0, $offset,'GBK');
         preg_match_all('/\d{12}|[\x7f-\xff]+/', $data, $city);
         $city = $city[0];
-//        halt($city);
 
         $list = [];
-        for ($j=0; $j < count($city) ; $j++) {
-            $child = $this->getDistrict($city[$j]);
-//            $list[] = [
-//                'code'=> substr($city[$j], 0,4),
-//                'name'=> $city[++$j],
-//                'pcode' => $code,
-//                'level' => 2
-//            ];
-            ++$j;
-            $list = array_merge($list,$child);
-//            if($j > 30) {
-//                break;
-//            }
+        foreach ($city as $k=>$v) {
+            if($k%2 == 0) {
+                $list[] = [
+                    'code' => substr($city[$k],0,4),
+                    'name' => $city[$k+1],
+                    'pcode' => $code,
+                    'level' => 2
+                ];
+                $list = array_merge($list,$this->getDistrict($city[$k]));
+            }
         }
 //        halt($list);
-//        $res = Db::table('mp_city')->insertAll($list);
+//        $res = Db::table('mp_city_bak')->insertAll($list);
 //        halt($res);
-        return $list;
     }
-/*
- * 4604 4419 4420
- * */
-//    public function getProvince()
-//    {
-////        $arr = [53];// 53
-////        $res = [];
-////        foreach ($arr as $v) {
-////            $res = array_merge($res,$this->getCity($v));
-////        }
-////        try {
-////            Db::table('mp_city')->insertAll($res);
-////        }catch (\Exception $e) {
-////            echo $e->getMessage();
-////        }
-////        halt($res);
-//
-//
-//
-//        $curl = curl_init();
-//        curl_setopt($curl, CURLOPT_URL, 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/index.html');
-//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-//        $data = curl_exec($curl);
-//        curl_close($curl);
-//        $data = mb_convert_encoding($data, 'UTF-8', 'GBK');
-//// 裁头
-//        $offset = mb_strpos($data, 'provincetr',2000,'GBK');
-//        $data = mb_substr($data, $offset,NULL,'GBK');
-//// 裁尾
-//        $offset = mb_strpos($data, '</TABLE>', 200,'GBK');
-//        $data = mb_substr($data, 0, $offset,'GBK');
-//        preg_match_all('/\d{2}|[\x7f-\xff]+/', $data, $out);
-//        $province = $out[0];
-//        halt($province);
-//
-//        $list = [];
-//        for ($j=0; $j < count($province) ; $j++) {
-//            $list[] = [
-//                'code' => $province[$j],
-//                'name' => $province[++$j],
-//                'pid' => 0,
-//                //'child' => $this->getCity($province[$j-1])
-//            ];
-//        }
-//        Db::table('mp_city')->insertAll($list);
+
+
+    public function getProvince()
+    {
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/index.html');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+        curl_close($curl);
+        $data = mb_convert_encoding($data, 'UTF-8', 'GBK');
+// 裁头
+        $offset = mb_strpos($data, 'provincetr',2000,'GBK');
+        $data = mb_substr($data, $offset,NULL,'GBK');
+// 裁尾
+        $offset = mb_strpos($data, '</TABLE>', 200,'GBK');
+        $data = mb_substr($data, 0, $offset,'GBK');
+        preg_match_all('/\d{2}|[\x7f-\xff]+/', $data, $out);
+        $province = $out[0];
+
+        $list = [];
+
+        foreach ($province as $k=>$v) {
+            if($k%2 == 0) {
+                $list[] = [
+                    'code' => $province[$k],
+                    'name' => $province[$k+1],
+                    'pcode' => 0,
+                    //'child' => $this->getCity($province[$j-1])
+                ];
+            }
+        }
+
 //        halt($list);
-//    }
-//
-//
+//        Db::table('mp_city_bak')->insertAll($list);
+    }
+
+
 //    public function test() {
-//        $this->getCity('44');
+//        $this->getCity('65');
 //    }
-//
-//    public function tests() {
-//        $this->getDistrict('4604');
-//    }
+
 
 }
